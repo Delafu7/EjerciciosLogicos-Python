d419cf8734075fcc92a80a9d4f516b6f44e97548
Delafu7 <delafuenteeneko022@gmail.com>
2025-01-03 16:11:44 +0100

    Create Abaco.py

---
+++ Nivel facil/Abaco.py
@@ -0,0 +1,30 @@
+"""/*
+ * Crea una función que sea capaz de leer el número representado por el ábaco.
+ * - El ábaco se representa por un array con 7 elementos.
+ * - Cada elemento tendrá 9 "O" (aunque habitualmente tiene 10 para realizar
+ *   operaciones) para las cuentas y una secuencia de "---" para el alambre.
+ * - El primer elemento del array representa los millones, y el último las unidades.
+ * - El número en cada elemento se representa por las cuentas que están a
+ *   la izquierda del alambre.
+ *
+ * Ejemplo de array y resultado:
+ * ["O---OOOOOOOO",
+ *  "OOO---OOOOOO",
+ *  "---OOOOOOOOO",
+ *  "OO---OOOOOOO",
+ *  "OOOOOOO---OO",
+ *  "OOOOOOOOO---",
+ *  "---OOOOOOOOO"]
+ *
+ *  Resultado: 1.302.790
+ */"""
+def leerAbaco(abaco:list)->int:
+    return int("".join([str(len(numero.split("---")[0])) for numero in abaco]))
+#Testeo de la función
+print(leerAbaco(["O---OOOOOOOO",
+                 "OOO---OOOOOO",
+                 "---OOOOOOOOO",
+                 "OO---OOOOOOO",
+                 "OOOOOOO---OO",
+                 "OOOOOOOOO---",
+                 "---OOOOOOOOO"]))
